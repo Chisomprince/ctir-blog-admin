3b8ea8f3196291a1aed22328f9274e824ccfa0e3
chisom prince <65718483+Chisomprince@users.noreply.github.com>
2021-11-14 00:49:21 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f1c203bccbc558dcae933da52f00ec1d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1869ca3e-efb5-455f-a908-093091aab785'
+};
